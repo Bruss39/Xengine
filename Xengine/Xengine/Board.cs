49ceb97cdb237ec02bc9885ca33936336b99bb93
Elia <158532198+Bruss39@users.noreply.github.com>
2024-02-13 21:56:13 +0100

    Create NonEmptySquares

--- Xengine/Xengine/Board.cs
+++ Xengine/Xengine/Board.cs
@@ -8,8 +8,35 @@ public class Board
     public static bool IsOpponentTurn = false;
 
 
-    //public static List<Move> GenerateAllMoves()
-    //{
+    public static List<Move> GenerateAllMoves()
+    {
+        List<Move> allMoves = new();
 
-    //}
+        foreach (Coordinate pos in NonEmptySquares())
+        {
+            allMoves.AddRange(GenerateMoves(pos));
+        }
+
+        return allMoves;
+    }
+
+    private static List<Move> GenerateMoves(Coordinate pos)
+    {
+        return new();
+    }
+
+    private static List<Coordinate> NonEmptySquares()
+    {
+        List<Coordinate> result = new();
+
+        for (int x = 0; x < 8; x++)
+        {
+            for (int y = 0; y < 8; y++)
+            {
+                if (Pieces[x, y] != Piece.Empty) result.Add(new(x, y));
+            }
+        }
+
+        return result;
+    }
 }
