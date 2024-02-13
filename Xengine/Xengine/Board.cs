ee8c1680f675baf886056eead9d922d9d42598a3
Elia <158532198+Bruss39@users.noreply.github.com>
2024-02-13 18:38:38 +0100

    Chess board representation

---
+++ Xengine/Xengine/Board.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Board
+{
+    public static int[,] Pieces = new int[8, 8];
+
+}
---
+++ Xengine/Xengine/Piece.cs
@@ -0,0 +1,18 @@
+﻿public struct Piece
+{
+    public const int Empty = 0;
+
+    public const int WhiteKing = 1;
+    public const int WhiteQueen = 2;
+    public const int WhiteBishop = 3;
+    public const int WhiteKinght = 4;
+    public const int WhiteRook = 5;
+    public const int WhitePawn = 6;
+
+    public const int BlackKing = 7;
+    public const int BlackQueen = 8;
+    public const int BlackBishop = 9;
+    public const int BlackKinght = 10;
+    public const int BlackRook = 11;
+    public const int BlackPawn = 12;
+}
