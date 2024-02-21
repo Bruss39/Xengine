4a4f6939873f3ac884bb7a2ecc52d3899180b230
Eliagiac <elia.giaccardi@gmail.com>
2024-02-21 11:34:24 +0100

    Refactor directions system
    - Rimosso le variabili locali "orthogonal" e "diagonal", che venivano inizializzate ad ogni chiamata della funzione GenerateMovesForPiece. Per rimpiazzarle ho aggiunto due collezioni costanti a Coordinate contenenti le direzioni ortogonali e diagonali, e una con entrambe le liste combinate in una.
    - Aggiunto funzioni Left, Right, Down e Up per spostare una coordinata in una data direzione. La lunghezza dello spostamento e determinata dal parametro length, con valore 1 di default.
    - Implementato operatori di addizione e sottrazione per modificare una coordinata in base a un dato spostamento. Funzionano in modo analogo all'addizione tra vettori.
    - Rimosso l'uso di Tuple e valori individuali per rappresentare una direzione o un movimento, in favore della struttura Coordinata.
    - Corretto il percorso del progetto nella soluzione da "Xengine\Xengine.csproj" a "src\Xengine.csproj".

--- src/Board.cs
+++ src/Board.cs
@@ -24,35 +24,33 @@ public class Board
     private static List<Move> GenerateMovesForPiece(Coordinate pos)
     {
         List<Move> moves = new();
-        List<Tuple<int, int>> orthogonal = new() {new(-1, 0), new(1, 0), new(0, 1), new(0, -1)};
-        List<Tuple<int, int>> diagonal = new() {new(-1, -1), new(1, -1), new(1, 1), new(-1, 1)};
 
         switch (Pieces[pos.X, pos.Y])
         {
             case Piece.WhitePawn:
                 // Se la posizione sopra e' libera, si puo' muovere.
-                if (IntExtensions.IsEmpty(new(pos.X, pos.Y - 1)))
+                if (IntExtensions.IsEmpty(pos.Up()))
                 {
-                    moves.Add(new(pos, new(pos.X, pos.Y - 1)));
+                    moves.Add(new(pos, pos.Up()));
 
                     // Se 2 posizioni sopra e' libero, si puo' muovere.
                     if (pos.Y == 6 && IntExtensions.IsEmpty(new(pos.X, pos.Y - 2)))
                     {
-                        moves.Add(new(pos, new(pos.X, pos.Y - 2)));
+                        moves.Add(new(pos, pos.Up(2)));
                     }
                 }
                 if (pos.X > 0 && Pieces[pos.X - 1, pos.Y - 1].IsBlack())
                 {
-                    moves.Add(new(pos, new(pos.X - 1, pos.Y - 1)));
+                    moves.Add(new(pos, pos.Left().Up()));
                     if (Pieces[pos.X - 1, pos.Y - 1].IsKing())
-                        KingEatenError(pos, new(pos.X - 1, pos.Y - 1));
+                        KingEatenError(pos, pos.Left().Up());
                 }
 
                 if (pos.X < 7 && Pieces[pos.X + 1, pos.Y - 1].IsBlack())
                 {
-                    moves.Add(new(pos, new(pos.X + 1, pos.Y - 1)));
+                    moves.Add(new(pos, pos.Right().Up()));
                     if (Pieces[pos.X + 1, pos.Y - 1].IsKing())
-                        KingEatenError(pos, new(pos.X + 1, pos.Y - 1));
+                        KingEatenError(pos, pos.Right().Up());
                 }
 
                 break;
@@ -62,8 +60,8 @@ public class Board
             case Piece.BlackRook:
                 List<Coordinate> movesToAddWhiteRook = new();
 
-                foreach (Tuple<int, int> tuple in orthogonal)
-                    movesToAddWhiteRook.AddRange(MoveVerify(tuple.Item1, tuple.Item2, pos));
+                foreach (Coordinate direction in Coordinate.OrthogonalDirections)
+                    movesToAddWhiteRook.AddRange(MoveVerify(direction, pos));
 
                 foreach (Coordinate elm in movesToAddWhiteRook)
                     moves.Add(new(pos, elm));
@@ -75,8 +73,8 @@ public class Board
             case Piece.BlackBishop:
                 List<Coordinate> movesToAddWhiteBishop = new();
 
-                foreach (Tuple<int, int> tuple in diagonal)
-                    movesToAddWhiteBishop.AddRange(MoveVerify(tuple.Item1, tuple.Item2, pos));
+                foreach (Coordinate direction in Coordinate.DiagonalDirections)
+                    movesToAddWhiteBishop.AddRange(MoveVerify(direction, pos));
 
                 foreach (Coordinate elm in movesToAddWhiteBishop)
                     moves.Add(new(pos, elm));
@@ -88,11 +86,8 @@ public class Board
             case Piece.BlackQueen:
                 List<Coordinate> movesToAddWhiteQueen = new();
 
-                foreach (Tuple<int, int> tuple in orthogonal)
-                    movesToAddWhiteQueen.AddRange(MoveVerify(tuple.Item1, tuple.Item2, pos));
-
-                foreach (Tuple<int, int> tuple in diagonal)
-                    movesToAddWhiteQueen.AddRange(MoveVerify(tuple.Item1, tuple.Item2, pos));
+                foreach (Coordinate direction in Coordinate.AllDirections)
+                    movesToAddWhiteQueen.AddRange(MoveVerify(direction, pos));
 
                 foreach (Coordinate elm in movesToAddWhiteQueen)
                     moves.Add(new(pos, elm));
@@ -127,15 +122,14 @@ public class Board
     }
 
 
-    private static List<Coordinate> MoveVerify(int addToX, int addToY, Coordinate position)
+    private static List<Coordinate> MoveVerify(Coordinate movement, Coordinate position)
     {
         List<Coordinate> movesToAdd = new();
         Coordinate pos = position;
 
         while (true)
         {
-            position.X += addToX;
-            position.Y += addToY;
+            position += movement;
 
             if (OffTheChessBoard(position))
             {
--- src/Coordinate.cs
+++ src/Coordinate.cs
@@ -1,6 +1,34 @@
-﻿public struct Coordinate
+﻿/// <summary>
+/// Struttura usata per rappresentare la posizione di un quadrato sulla scacchiera, una direzione o un movimento.
+/// </summary>
+/// <remarks>
+/// Le coordinate vanno dall'angolo in alto a sinistra a quello in basso a destra, perciò aggiungere alla y risulta in uno spostamento verso il basso.
+/// </remarks>
+public struct Coordinate
 {
     public static string[] FileLetter = { "a", "b", "c", "d", "e", "f", "g", "h"};
+
+
+    public static readonly Coordinate LeftDirection = new(-1, 0);
+
+    public static readonly Coordinate RightDirection = new(-1, 0);
+
+    public static readonly Coordinate DownDirection = new(0, 1);
+
+    public static readonly Coordinate UpDirection = new(0, -1);
+
+
+    public static readonly List<Coordinate> OrthogonalDirections = new() { LeftDirection, RightDirection, DownDirection, UpDirection };
+
+    public static readonly List<Coordinate> DiagonalDirections = new() { 
+        LeftDirection   + UpDirection, 
+        RightDirection  + UpDirection, 
+        LeftDirection   + DownDirection, 
+        RightDirection  + DownDirection 
+    };
+
+    public static readonly List<Coordinate> AllDirections = OrthogonalDirections.Concat(DiagonalDirections).ToList();
+
 
     public int X;
     public int Y;
@@ -12,5 +40,18 @@
     }
 
 
+    public Coordinate Left(int length = 1) => new(X - length, Y);
+
+    public Coordinate Right(int length = 1) => new(X + length, Y);
+
+    public Coordinate Down(int length = 1) => new(X, Y + length);
+
+    public Coordinate Up(int length = 1) => new(X, Y - length);
+
+
     public override string ToString() => $"{FileLetter[X]}{8 - Y}";
+
+    public static Coordinate operator +(Coordinate left, Coordinate right) => new(left.X + right.X, left.Y + right.Y);
+
+    public static Coordinate operator -(Coordinate left, Coordinate right) => new(left.X - right.X, left.Y - right.Y);
 }
