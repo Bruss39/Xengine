6e5badfe5b51b1b5741bf3eef6cb15240fef9bab
Elia <158532198+Bruss39@users.noreply.github.com>
2024-02-23 20:43:44 +0100

    Add tests project

--- src/Piece.cs
+++ src/Piece.cs
@@ -2,17 +2,17 @@
 {
     public const int Empty = 0;
 
-    public const int WhiteKing = 1;
-    public const int WhiteQueen = 2;  // Ok.
+    public const int WhiteKing   = 1;
+    public const int WhiteQueen  = 2;  // Ok.
     public const int WhiteBishop = 3;  // Ok.
     public const int WhiteKnight = 4;  // Ok.
-    public const int WhiteRook = 5;  // Ok.
-    public const int WhitePawn = 6;  // Ok.
+    public const int WhiteRook   = 5;  // Ok.
+    public const int WhitePawn   = 6;  // Ok.
 
-    public const int BlackKing = 7;
-    public const int BlackQueen = 8;
-    public const int BlackBishop = 9;
-    public const int BlackKnight = 10;
-    public const int BlackRook = 11;
-    public const int BlackPawn = 12;
+    public const int BlackKing   = 7;
+    public const int BlackQueen  = 8;  // Ok.
+    public const int BlackBishop = 9;  // Ok.
+    public const int BlackKnight = 10; // Ok.
+    public const int BlackRook   = 11; // Ok.
+    public const int BlackPawn   = 12;
 }
